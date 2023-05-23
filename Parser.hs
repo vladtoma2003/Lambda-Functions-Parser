0b9c91e993b65697dbc30ffd3016394b13e71cb4
Vlad Iliescu <vladtoma.iliescu@gmail.com>
2023-05-23 13:11:12 +0300

    parser

--- Parser.hs
+++ Parser.hs
@@ -3,6 +3,11 @@ module Parser (parse_expr, parse_code) where
 import Control.Monad
 import Control.Applicative
 import Expr
+import Data.Char (isAlphaNum, isAlpha)
+import Data.Data (isAlgType)
+import GHC.Base (Alternative((<|>)))
+import GHC.Exts.Heap.Closures (GenClosure(var, APClosure))
+import GHC.Read (paren)
 
 -- Parser data type
 newtype Parser a = Parser {
@@ -12,15 +17,20 @@ newtype Parser a = Parser {
 --- type declaration ---
 
 instance Monad Parser where
-    return = undefined
-    (>>=) = undefined
+    return x = Parser $ \s -> Just (x, s)
+
+    -- (>>=) = 
+    mp >>= f =
+        Parser $ \s ->
+            case parse mp s of
+                Nothing -> Nothing
+                Just (x, s) -> parse (f x) s
 
 instance Applicative Parser where
-    pure x = return x
     pf <*> px = do
         f <- pf
-        x <- px
-        return $ f x
+        f <$> px
+    pure = return
 
 instance Functor Parser where
     fmap f px = do
@@ -31,7 +41,75 @@ instance Functor Parser where
 
 -- TODO 2.1. parse a expression
 parse_expr :: String -> Expr
-parse_expr = undefined
+parse_expr cv = 
+    case parse initialParser cv of
+        Nothing -> error "parse error"
+        Just (e, _) -> e
+
+initialParser :: Parser Expr
+initialParser = applicationParser <|> atomParser
+
+atomParser :: Parser Expr
+atomParser = functionParser <|> variableParser <|> parenParser
+
+instance Alternative Parser where
+    empty = Parser (const Nothing)
+    (Parser p1) <|> (Parser p2) = Parser (\s -> case p1 s of
+                                Nothing -> p2 s
+                                ok -> ok)
+
+charParser :: Char -> Parser Char
+charParser c = Parser (\s ->
+    case s of
+        [] -> Nothing
+        (x:xs) -> if x == c then Just (c, xs) else Nothing)
+
+predicateParser :: (Char -> Bool) -> Parser Char -- aplica o functie 
+predicateParser p = Parser (\s ->
+    case s of
+        [] -> Nothing
+        (x:xs) -> if p x then Just (x, xs) else Nothing)
+
+starParser :: Parser a -> Parser [a] -- asta
+starParser p = plusParser p <|> return []  -- <|> alternativa
+
+plusParser :: Parser a -> Parser [a]
+plusParser p =
+    do  x <- p
+        xs <- starParser p
+        return (x:xs)
+
+varParser :: Parser String -- asta
+varParser =
+    do  x <- predicateParser isAlpha
+        xs <- many (predicateParser isAlphaNum)
+        return (x:xs)
+
+variableParser :: Parser Expr
+variableParser =
+    do  x <- varParser
+        return (Variable x)
+
+functionParser :: Parser Expr
+functionParser =
+    do  charParser '\\'
+        x <- varParser
+        charParser '.'
+        e <- initialParser
+        return (Function x e)
+
+applicationParser :: Parser Expr
+applicationParser =
+    do  e1 <- atomParser
+        rest <- many (charParser ' ' *> atomParser)
+        return (foldl Application e1 rest)
+    
+parenParser :: Parser Expr
+parenParser =
+    do  charParser '('
+        e <- applicationParser
+        charParser ')'
+        return e
 
 -- TODO 4.2. parse code
 parse_code :: String -> Code
--- zzzt.hs
+++ zzzt.hs
@@ -7,12 +7,12 @@ data Expr = Variable String
 
 instance Show Expr where
     show (Variable x) = x
-    show (Function x (Application e1 e2)) = ('λ':x) ++ ".(" ++ (show e) ++ ")"
-        where e = (Application e1 e2)
-    show (Function x e) = ('λ':x) ++ ('.':(show e))
-    show (Application e1 (Application u v)) = (show e1) ++ " (" ++ (show e2) ++ ")"
-        where e2 = (Application u v)
-    show (Application e1 e2) = (show e1) ++ (' ':(show e2))
+    show (Function x (Application e1 e2)) = ('λ':x) ++ ".(" ++ show e ++ ")"
+        where e = Application e1 e2
+    show (Function x e) = ('λ':x) ++ ('.':show e)
+    show (Application e1 (Application u v)) = show e1 ++ " (" ++ show e2 ++ ")"
+        where e2 = Application u v
+    show (Application e1 e2) = show e1 ++ (' ':show e2)
 
 free_vars :: Expr -> [String]
 free_vars =
@@ -26,12 +26,12 @@ instance Eq Expr where
     (==) e1 e2 = equal e1 e2 []
       where
         equal :: Expr -> Expr -> [(String, String)] -> Bool
-        equal (Variable x) (Variable y) env = case (lookup x env) of 
+        equal (Variable x) (Variable y) env = case lookup x env of
                                                 (Just xv) -> xv == y
                                                 Nothing -> x == y
 
         equal (Function x e1) (Function y e2) env = equal e1 e2 ((x,y):env)
-        equal (Application e1 e2) (Application e3 e4) env = (equal e1 e3 env) && (equal e2 e4 env)
+        equal (Application e1 e2) (Application e3 e4) env = equal e1 e3 env && equal e2 e4 env
         -- TODO 3. add equal instance for Macro
         -- before default case !!!
         equal _ _ _ = False
@@ -51,19 +51,19 @@ ans = f "y" (a (f "x" vy) vy)
 
 alphabet = ['a'..'z']
 
-transformation :: [Char] -> [String]
-transformation xs = map (\x -> [x]) xs
+transformation :: String -> [String]
+transformation = map (\x -> [x])
 
 filterOut :: [String] -> [String] -> [String]
-filterOut xs ys = filter (\x -> not (elem x ys)) xs
+filterOut xs ys = filter (\x -> notElem x ys) xs
 
 replace :: [String] -> Expr -> Expr
 replace freeVars e =
     case e of
         (Variable x) ->
-            if elem x freeVars then Variable (head (filterOut (transformation alphabet) freeVars)) else e
+            if x `elem` freeVars then Variable (head (filterOut (transformation alphabet) freeVars)) else e
         (Function x e1) ->
-            if elem x freeVars then
+            if x `elem` freeVars then
                 let
                     newVar = head (filterOut (transformation alphabet) freeVars)
                     -- newFreeVars = newVar : freeVars
@@ -83,4 +83,6 @@ letters = concatMap (\n -> map (take n) $ tails ['a'..'z']) [1..]
 charList = concatMap (\n -> map (take n) $ tails ['a'..'z']) [1..]
 
 cv = concatMap (\n -> sequence (take n (tails ['a' .. 'z']))) [1..]
+
+
 
